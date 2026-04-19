946aee0ee872a931fcc4ed56a23a60e5dab5d6de
Oscar <oscmog@gmail.com>
2026-04-19 17:57:35 +0100

    Added main menu

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,3 +4,12 @@ extends Control
 
 func _on_play_button_pressed() -> void:
     get_tree().change_scene_to_packed(play_scene)
+    
+func _on_credits_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_settings_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+    get_tree().quit()
